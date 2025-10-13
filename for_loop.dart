336c4cee775c4fd3ed1eb29fba09495552c595ca
yopiaditia <yopiaditiaramadan24@gmail.com>
2025-10-13 12:16:19 +0700

    update main.dart

---
+++ for_loop.dart
@@ -0,0 +1,10 @@
+void main() {
+  for (var counter = 0; counter < 5; counter++) {
+    print('Counter is $counter');
+  }
+
+  List<String> fruits = ['apple', 'banana', 'orange'];
+  for (String fruit in fruits) {
+    print(fruit);
+  }
+}
